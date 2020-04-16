3e36d548c764ad5d043165da1cc3d073d54941d5
Evan Sarmiento <esarmien@g.harvard.edu>
2020-04-16 05:27:15 -0400

    Add tests.

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const config = require('config');
 const vhost = require('@tryghost/vhost-middleware');
 const url = require('url');
 const querystring = require('querystring');
-
+const slashes = require('remove-trailing-slash');
 const app = express();
 const defaultRedirect = config.get('defaultRedirect');
 const redirectMap = config.get('redirectMap');
 
-const makeDst = (u, qu) => (qu && `${u}?${qu}` || u);
+const makeDst = (u, p, qu) => (qu && `${u}${p === '/' && p || ''}?${qu}` || u);
 
 for (const {src, dst, withPath, withQs} of redirectMap) {
     app.use(vhost(RegExp(src), ({path, query}, res) => {
-        const u = withPath === false && dst || url.resolve(dst, path) || dst;
-        const dstUrl = withQs === false && u || makeDst(u, querystring.stringify(query))
+        const u = slashes(withPath === false && dst || url.resolve(dst, path) || dst);
+        const dstUrl = withQs === false && u || makeDst(u, path, querystring.stringify(query))
         res.redirect(dstUrl);
     }));
 }
@@ -22,4 +22,7 @@ app.use('*', (req, res) => {
     return res.redirect(301, defaultRedirect);
 })
 
-app.listen(3000);
+module.exports = { 
+    app, 
+    http: app.listen(3000) 
+}
---
+++ tests/riri.test.js
@@ -0,0 +1,142 @@
+const got = require('got');
+const assert = require('assert');
+const { certificateFor } = require('devcert');
+const https = require('https');
+
+describe('http request', function() {
+    var App;
+    var Http;
+
+    this.timeout(5000);
+
+    before(function () {
+        const { app, http } = require('../index');
+        App = app;
+        Http = http;
+    });
+
+    it('method GET HOST http://www.one.pub should 301 redirect to https://another.org', async function() {
+        const { headers } = await got(
+            'http://localhost:3000',
+            { 
+                method: 'GET',
+                headers: { host: 'www.one.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://another.org');
+    });
+
+    it('method GET HOST http://x.one.pub should 301 redirect to https://another.org', async function() {
+        const { headers } = await got(
+            'http://localhost:3000',
+            { 
+                method: 'GET',
+                headers: { host: 'x.one.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://another.org');
+    });
+
+    it('method GET HOST http://coolone.pub should 301 redirect to https://www.proxywow.com', async function() {
+        const { headers } = await got(
+            'http://localhost:3000',
+            { 
+                method: 'GET',
+                headers: { host: 'coolone.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://www.proxywow.com');
+    });
+
+    it('method GET HOST http://one.pub/this/place should 301 redirect to https://another.org/this/place, path included by default.', async function() {
+        const { headers } = await got(
+            'http://localhost:3000/this/place',
+            { 
+                method: 'GET',
+                headers: { host: 'one.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://another.org/this/place');
+    });
+
+
+    it('method GET HOST http://one.pub/?test=1 should 301 redirect to https://another.org/?test=1, query included by default.', async function() {
+        const { headers } = await got(
+            'http://localhost:3000/?test=1',
+            { 
+                method: 'GET',
+                headers: { host: 'one.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://another.org/?test=1');
+    });
+
+    it('method GET HOST http://one.pub/a/b/c?test=1 should 301 redirect to https://another.org/a/b/c?test=1, query included by default.', async function() {
+        const { headers } = await got(
+            'http://localhost:3000/a/b/c?test=1',
+            { 
+                method: 'GET',
+                headers: { host: 'one.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://another.org/a/b/c?test=1');
+    });
+
+    it('method GET HOST http://two.pub/a/b/c should 301 redirect to https://line.struggle with withPath==false', async function() {
+        const { headers } = await got(
+            'http://localhost:3000/a/b/c',
+            { 
+                method: 'GET',
+                headers: { host: 'two.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://line.struggle');
+    });
+
+
+    it('method GET HOST http://three.pub/a/b/c?q=1 should 301 redirect to https://in.one/a/b/c with withQs==false', async function() {
+        const { headers } = await got(
+            'http://localhost:3000/a/b/c?q=1',
+            { 
+                method: 'GET',
+                headers: { host: 'three.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://in.one/a/b/c');
+    });
+    
+
+    it('method GET HOST http://four.pub/a/b/c?q=1 should 301 redirect to https://unit.cell with withQs==false and withPath==false', async function() {
+        const { headers } = await got(
+            'http://localhost:3000/a/b/c?q=1',
+            { 
+                method: 'GET',
+                headers: { host: 'three.pub' },
+                followRedirect: false
+            }
+        );
+
+        return assert.equal(headers.location, 'https://in.one/a/b/c');
+    });
+
+
+    after(function () {
+        Http.close();
+    });
+})
